8575d986d01ed626b141a310591e99b8a90bd932
oveja87 <katrinh1987@web.de>
2012-01-09 12:58:12 +0100

    harmonies.js changed

---
+++ harmonies.js
@@ -0,0 +1,25 @@
+
+(function( $) {
+$.widget( "ui.harmonies", {
+	
+	options: {
+		hex: '45ade2'
+	},
+	
+	_create: function() {		
+			
+	},
+	
+	setOptions: function()
+	{
+		
+	},
+	
+	destroy: function() {   	  
+	}
+})
+
+
+
+
+})( jQuery );
